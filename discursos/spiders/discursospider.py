3568b56758541cfaeafab5b02ac9ffc20b961361
jdieguez1 <jdieguez@fi.uba.ar>
2024-03-06 16:22:33 -0300

    Actualización de archivo .CSV hasta última fecha registrada

--- discursos/spiders/discursospider.py
+++ discursos/spiders/discursospider.py
@@ -1,46 +1,21 @@
-import scrapy, re, csv
+import scrapy, re, csv, json
 from datetime import datetime
+from scrapy.exceptions import CloseSpider
 
-presidentes_info = [
-    {
-        'presidente': 'Mauricio Macri',
-        'desde': '10/12/2015',
-        'hasta': '10/12/2019'
-    },
-    {
-        'presidente': 'Alberto Fernández',
-        'desde': '10/12/2019',
-        'hasta': 'Presente'
-    }
-]
+URL_CASA_ROSADA = "www.casarosada.gob.ar"
+URL_DISCURSOS = "https://www.casarosada.gob.ar/informacion/discursos"
 
-presidentes = ["Mauricio Macri", "Alberto Fernández"]
-meses = {
-            'enero': '01',
-            'febrero': '02',
-            'marzo': '03',
-            'abril': '04',
-            'mayo': '05',
-            'junio': '06',
-            'julio': '07',
-            'agosto': '08',
-            'septiembre': '09',
-            'octubre': '10',
-            'noviembre': '11',
-            'diciembre': '12'
-        }
-
-def obtener_orador(titulo, fecha):    
-    orador = next((presidente for presidente in presidentes if presidente in titulo), 'Indefinido')
+def obtener_orador(titulo, fecha, presidentes):    
+    orador = next((presidente['nombre'] for presidente in presidentes if presidente['nombre'] in titulo), 'Indefinido')
     
     if orador == 'Indefinido' and fecha is not None:
-        for info in presidentes_info:
+        for info in presidentes:
             desde = datetime.strptime(info['desde'], '%d/%m/%Y')
             hasta = datetime.now() if info['hasta'] == 'Presente' else datetime.strptime(info['hasta'], '%d/%m/%Y')
             fecha_discurso = datetime.strptime(fecha, '%d/%m/%Y')
 
             if desde <= fecha_discurso <= hasta:
-                orador = info['presidente']
+                orador = info['nombre']
                 break
 
     return orador
@@ -51,7 +26,7 @@ def limpiar_pagrafo(pagrafo):
         pagrafo = pagrafo.replace(i, '')
     return pagrafo
 
-def parsear_fecha(fecha):
+def parsear_fecha(fecha, meses):
     # Definir el patrón de expresión regular para extraer la fecha
     patron = r'(\w+)\s(\d+) de (\w+) de (\d+)'
     matches = re.search(patron, fecha, re.IGNORECASE)
@@ -66,12 +41,23 @@ def parsear_fecha(fecha):
 
 class DiscursoSpider(scrapy.Spider):
     name = "discursospider"
-    allowed_domains = ["www.casarosada.gob.ar"]
-    start_urls = ["https://www.casarosada.gob.ar/informacion/discursos"]
-    
+    allowed_domains = [URL_CASA_ROSADA]
+    start_urls = [URL_DISCURSOS]
+
     def __init__(self, *args, **kwargs):
         super(DiscursoSpider, self).__init__(*args, **kwargs)
         self.data_list = []
+
+        with open('fecha.txt', 'r') as f:
+            self.ultima_actualizacion = datetime.strptime(f.readline().strip(), '%d/%m/%Y')
+
+        self.nueva_actualizacion = self.ultima_actualizacion
+
+        with open('presidentes.json', 'r') as file:
+            self.presidentes = json.load(file)
+
+        with open('meses.json', 'r') as file:
+            self.meses = json.load(file)
 
     def parse(self, response):
         items = response.css('div.item[itemprop="blogPost"][itemscope][itemtype="http://schema.org/BlogPosting"]')
@@ -90,26 +76,36 @@ class DiscursoSpider(scrapy.Spider):
         texto = [limpiar_pagrafo(pagrafo) for pagrafo in pagrafos if pagrafo.strip()]
         texto_final = ' '.join(texto)
         
-        fecha = parsear_fecha(response.css('time.pull-right::text').get())
+        fecha = parsear_fecha(response.css('time.pull-right::text').get(), self.meses)
+        fecha_datetime = datetime.strptime(fecha, '%d/%m/%Y')
         titulo = response.css('title::text').get()
+        orador = obtener_orador(titulo, fecha, self.presidentes)
 
-        orador = obtener_orador(titulo, fecha)
+        if fecha_datetime > self.ultima_actualizacion: 
+            if fecha_datetime > self.nueva_actualizacion:
+                self.nueva_actualizacion = fecha_datetime
 
-        if (orador != "Indefinido") and (len(texto_final) > 0):
-            data = {
-                'orador': orador,
-                'fecha': fecha,
-                'discurso': texto_final
-            }
-        
-            self.data_list.append(data)
-            yield data
+            if (orador != "Indefinido") and (len(texto_final) > 0):
+                data = {
+                    'orador': orador,
+                    'fecha': fecha,
+                    'discurso': texto_final
+                }
+            
+                self.data_list.append(data)
+                yield data
+
+        else:
+            raise CloseSpider('Actualización terminada')
 
     def close(self, reason):
-        with open('discursos.csv', mode='w', newline='', encoding='utf-8') as file:
+        with open('discursos.csv', mode='a', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=['orador', 'fecha', 'discurso'])
-            writer.writeheader()
             writer.writerows(self.data_list)
+
+        if self.nueva_actualizacion is not None and self.nueva_actualizacion > self.ultima_actualizacion:
+            with open('fecha.txt', 'w') as f:
+                f.write(self.nueva_actualizacion.strftime('%d/%m/%Y'))
 
 if __name__ == "__main__":
     process = CrawlerProcess()
