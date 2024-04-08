e93c708a5e189c5f4bdf8312bc36ae258298d31e
jdieguez1 <jdieguez@fi.uba.ar>
2024-04-08 13:36:39 -0300

    Fix directorio para guardar imagenes de las nubes de palabras

--- nube_de_palabras.py
+++ nube_de_palabras.py
@@ -13,7 +13,7 @@ data = pd.read_csv('discursos.csv')
 oradores_unicos = data['orador'].unique()
 plt.figure(figsize=(10, 5))
 
-directorio_img = os.path.join(os.getcwd(), '/images')
+directorio_img = os.path.join(os.getcwd(), '/app/public/images')
 
 def guardar_todas_las_palabras(orador, texto):
     WordCloud(width=800, height=400, background_color='white') \
