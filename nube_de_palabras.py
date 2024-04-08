a066115c3c2a5b386a3887954160a5b497333a76
jdieguez1 <jdieguez@fi.uba.ar>
2024-04-08 13:01:14 -0300

    Instalar punkt

--- nube_de_palabras.py
+++ nube_de_palabras.py
@@ -6,13 +6,14 @@ import nltk
 from nltk.corpus import stopwords
 from textblob import TextBlob
 
+nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = set(stopwords.words('spanish'))
 data = pd.read_csv('discursos.csv')
 oradores_unicos = data['orador'].unique()
 plt.figure(figsize=(10, 5))
 
-directorio_img = os.path.join(os.getcwd(), 'app/public/images')
+directorio_img = os.path.join(os.getcwd(), '/images')
 
 def guardar_todas_las_palabras(orador, texto):
     WordCloud(width=800, height=400, background_color='white') \
