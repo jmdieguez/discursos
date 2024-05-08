85292e733e6f37fcce2a3711d9cd42e6b03d6908
jdieguez1 <jdieguez@fi.uba.ar>
2024-05-08 01:09:11 -0300

    Créditos y About

---
+++ app/src/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './About.css';
+function About() {
+  return (
+    <div className='about-content'>
+        <img src="https://lens-storage.storage.googleapis.com/png/04ea97857cb54c5f8b5856b4345605e1" 
+            alt="Description of the image" />
+
+        <h1>
+            Argentina: Análisis Lingüistico de presidentes a través del tiempo
+        </h1>
+
+        <h3>
+            No es ninguna novedad hoy en día, mayo de 2024, que Argentina viene sufriendo una crisis económica constante
+            hace años, por lo que analizar los discursos presidenciales de Argentina a lo largo del tiempo es fundamental 
+            para comprender la evolución política y social del país. Estos discursos reflejan las prioridades, 
+            valores y estrategias de cada gobierno, así como las preocupaciones y aspiraciones de la sociedad 
+            en diferentes momentos históricos. Mediante la creación de nubes de palabras y análisis de sentimientos, 
+            como alegría, enojo, miedo, sorpresa, disgusto y tristeza, podemos identificar patrones emocionales y 
+            discursivos, revelando tendencias, cambios de discurso y la conexión entre la retórica política 
+            y el estado de ánimo de la población. Este enfoque nos permite profundizar en el análisis de la 
+            comunicación política y entender mejor cómo los líderes influyen y responden a las emociones 
+            colectivas de la nación.
+        </h3>
+
+        <h2>
+          ¿Cuáles son las palabras más usadas por cada presidente? <br/>
+          ¿Cuáles son o fueron sus prioridades? <br/>
+          ¿A qué eventos históricos se pueden atribuir grandes cambios emocionales de cierto presidente a partir de una fecha?
+        </h2>
+
+        <a href="#informe">
+          <button type="button" class="btn btn-dark">
+            Ir al informe
+          </button>
+        </a>
+    </div>
+  );
+}
+
+export default About;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
+import About from './About'
 import presidentesData from './presidentes.json';
 import Presidente from './Presidente';
 function App() {
   return (
-    <div className='body'>
-      {presidentesData.map((presidente, index) => (
-        <Presidente presidente={presidente} key={presidente.nombre} index={index}/>
-      ))}
-    </div>
+    <>
+      <div className='header'>
+        <a href="https://github.com/jmdieguez/discursos" target="_blank"><i class="bi bi-github"></i> Ir al repositorio de GitHub</a>
+      </div>
+
+      <div className='body'>
+        <About/>
+        <div id='informe'>
+          {presidentesData.map((presidente, index) => (
+            <Presidente presidente={presidente} key={presidente.nombre} index={index}/>
+          ))}
+        </div>
+
+        <div className='footer'>
+          <div className='footer-credits'>
+            <i class="bi bi-code-slash"></i> 
+            <h6> jmdieguez - 2024</h6>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
